a248d57c26c83711ed9798231b24e6e731698255
kittipong_kam@true-e-logistics.com <kittipong_kam@true-e-logistics.com>
2021-01-28 13:44:43 +0700

    update travis

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> hi there!!!!
+          Edit <code>src/App.js</code> hello world!!!!
         </p>
         <a
           className="App-link"
